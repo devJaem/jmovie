1f3667f71c3c22ae0b7465ad406654248beb03b5
jaemin <wjdwoals9292@gmail.com>
2024-04-26 16:31:23 +0900

    [REFACTOR] 주석정리

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchInput = document.getElementById('search-text');
 const searchButton = document.getElementById('search-button');
 let allMovies = [];
 
+// API 정보
 const options = {
     method: 'GET',
     headers: {
@@ -11,13 +12,13 @@ const options = {
     }
 };
 
-// api 정보를 받아와서 showMovies()실행 
+// API 정보 받아오기
 fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(data => {
         allMovies = data.results;
         showMovies(allMovies);
-    }) // 모든 데이터를 배열에 저장
+    })
     .catch(err => console.error(err));
 
 // 카드를 생성하는 함수
@@ -31,24 +32,16 @@ const createMovie = movie => {
     const overviewElement = document.createElement('p');
     const voteAverageElement = document.createElement('p');
     
-    card.className = 'card mb-4'; 
-    //부트스트랩 여백
-    
     image.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
     image.className = "card-img-top";
     image.alt = `${name} Poster`;
-
     cardBody.className = 'card-body';
-
     titleElement.className = 'card-title';
     titleElement.textContent = name;
-
     overviewElement.className = 'card-text';
     overviewElement.textContent = overview;
-
     voteAverageElement.className = 'text-muted';
     voteAverageElement.textContent = `Rating: ${vote_average}/10`;
-
 
     cardBody.appendChild(titleElement);
     cardBody.appendChild(overviewElement);
@@ -62,17 +55,15 @@ const createMovie = movie => {
 
 //받아온 자료만큼 카드를 그리는 함수 
 const showMovies = movies => {
-    cardContainer.innerHTML = ''; // 기존 카드 삭제
+    cardContainer.innerHTML = '';
     
     movies.forEach(movie => {
         const movieCard = createMovie(movie);
         const colDiv = document.createElement('div');
         colDiv.className = 'col-lg-4';
-        //부트스트랩 카드 배치갯수
         colDiv.appendChild(movieCard);
         cardContainer.appendChild(colDiv);
 
-        // 카드 클릭 이벤트 리스너
         movieCard.addEventListener('click', () =>{
             const movieId = movieCard.getAttribute('id');
             alert(`Movie ID : ${movieId}`);
@@ -86,7 +77,6 @@ const searchMovies = () => {
     const filteredMovies = allMovies.filter(movie =>
         movie.name && movie.name.toLowerCase().includes(searchItem)
     );
-    console.log(filteredMovies); // 필터링된 결과 확인
     showMovies(filteredMovies);
 };
 
@@ -100,5 +90,5 @@ searchInput.addEventListener('keyup', event => {
 
 // 검색 입력란 커서
 window.onload = () => {
-    searchInput.focus(); // 페이지 로드 시 검색 입력란에 포커스 주기
+    searchInput.focus(); 
 };
